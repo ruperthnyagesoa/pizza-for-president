ae7a1b9024973d5d3fa51059e86987dbf3d50980
ruperthnyagesoa <ruperthnyagesoa@gmail.com>
2024-03-02 22:42:46 +0300

    ft:updated sweets

--- src/components/Products/data.js
+++ src/components/Products/data.js
@@ -3,7 +3,10 @@ import product2 from '../../images/product-2.jpg';
 import product3 from '../../images/product-3.jpg';
 import sweet1 from '../../images/sweet3.jpg';
 import sweet2 from '../../images/sweet-2.jpg';
+import sweet6 from '../../images/sweet2.jpg';
 import sweet3 from '../../images/sweet-3.jpg';
+import sweet5 from '../../images/sweet-1.jpg';
+import sweet7 from '../../images/sweet.jpg';
 
 export const productData = [
 	{
@@ -38,7 +41,7 @@ export const productDataTwo = [
 		alt: 'Doughnuts',
 		name: 'Doughlicious',
 		desc: 'Belgian chocolate glazed doughnuts covered in icing with sprinkles on top',
-		price: 'Ksh 100',
+		price: 'Ksh 420',
 		button: 'Add to Cart',
 	},
 	{
@@ -46,7 +49,7 @@ export const productDataTwo = [
 		alt: 'Ice Cream',
 		name: 'Caramel Wonder',
 		desc: 'Vanilla ice cream covered with caramel and chocolate glaze topped with a coco stick',
-		price: 'Ksh 130',
+		price: 'Ksh 320',
 		button: 'Add to Cart',
 	},
 	{
@@ -54,7 +57,32 @@ export const productDataTwo = [
 		alt: 'Brownie',
 		name: 'Brownie Bunch',
 		desc: 'Double fudge brownie squares topped with white chocolate pieces and macadamia nuts',
-		price: 'Ksh 100',
+		price: 'Ksh 220',
 		button: 'Add to Cart',
 	},
+	{
+		img: sweet6,
+		alt: 'Cookie',
+		name: 'Chocolate Sandwich',
+		desc: 'Espresso and luscious creamy filling, chocolatey, fudgy, lightly sweet and a little addictive!',
+		price: 'Ksh 330',
+		button: 'Add to Cart',
+	},
+	{
+		img: sweet5,
+		alt: 'Drink',
+		name: 'Hot Fudge',
+		desc: 'Hot fudge sauce for your next ice cream party.',
+		price: 'Ksh 400',
+		button: 'Add to Cart',
+	},
+	{
+		img: sweet7,
+		alt: 'Fudge',
+		name: 'Christmas Candy Cane Fudge',
+		desc: 'A fudge for a perfect holiday dessert',
+		price: 'Ksh 440',
+		button: 'Add to Cart',
+	},
+
 ];
